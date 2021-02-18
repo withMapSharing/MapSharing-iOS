ea19731508ea8f75e754b1f977d4b852bc62e29a
Taehyeon-Kim <taehyk528@gmail.com>
2021-02-18 18:32:36 +0900

    init: 프로젝트 폴더링

---
+++ MapSharing/MapSharing/Source/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapSharing
+//
+//  Created by taehy.k on 2021/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
